d1a66cec838ad8d88b09c67ab0f18eae3c03628b
rodrigoromano23 <rr699785@gmail.com>
2025-09-09 01:15:21 -0300

    rt

--- src/routes/invitacionesRoutes.mjs
+++ src/routes/invitacionesRoutes.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import { mostrarFormulario, confirmarAsistencia } from "../controllers/invitacionesController.mjs";
+import { mostrarFormulario, confirmarAsistencia } from "./controllers/invitacionesController.mjs"
 
 const router = express.Router();
 
